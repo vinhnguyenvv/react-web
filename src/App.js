6b5ee981cd9b027f691d045cf82580ab02f8a767
DESKTOP-F6U26OS\vinhn <vinhnguyenvv@gmail.com>
2018-09-19 10:58:20 +0700

    text

--- src/App.js
+++ src/App.js
@@ -32,7 +32,7 @@ class App extends Component {
                         <Route path="/" component={Home} exact />
                         <Route path="/home" component={Home} />
                         <Route path="/about" component={About} />
-                        <Route component={Error ssds} />
+                        <Route component={Error fdsfdfds } />
                     </Switch>
                 </div>
             </Router>
